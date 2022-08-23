17b06299a6144997fcf8fab1dc488ae868ed92e6
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-08-23 19:15:12 +0200

    Up date function Jogar() e Reset()

--- js/index.js
+++ js/index.js
@@ -1,3 +1,55 @@
 //manipulando o dom
-
 //instanciando nossa classe Game
+
+function jogar(){
+    if(document.getElementById("pedra").checked == false &&
+     document.getElementById("papel").checked == false &&
+     document.getElementById("tesoura").checked == false) {
+        alert("Selecione uma opcao: Pedra, Papel ou Tesoura");
+    } else {
+        let ramPc = Math.floor(Math.random() * 3) ;
+        console.log(ramPc);
+        switch(ramPc) {
+            case 0: document.getElementById("pc").src="../assets/PcPedra.png";
+            break;
+            case 1: document.getElementById("pc").src="../assets/PcPapel.png";
+            break;
+            case 2: document.getElementById("pc").src="../assets/PcTesoura.png";
+            break;
+        }
+        
+
+        //verificar o vencedor
+        if((document.getElementById("pedra").checked == true && ramPc == 0) ||
+        (document.getElementById("papel").checked == true && ramPc == 1) ||
+        (document.getElementById("tesoura").checked == true && ramPc == 2)) {
+            document.getElementById("placar").innerHTML="Empate";
+
+        } else if ((document.getElementById("pedra").checked == true && ramPc == 2) ||
+        (document.getElementById("papel").checked == true && ramPc == 0) ||
+        (document.getElementById("tesoura").checked == true && ramPc == 1)) {
+            document.getElementById("placar").innerHTML="Jogador Venceu";
+        
+        } else {
+            document.getElementById("placar").innerHTML="Computador Venceu"; 
+        }
+        
+        
+    }
+    
+}
+    
+
+function reset(){
+    document.getElementById("pc").src="../assets/PC.png"
+    document.getElementById("placar").innerHTML="";
+      
+    document.getElementById("pedra").checked = false;
+    document.getElementById("papel").checked = false;
+    document.getElementById("tesoura").checked = false;
+
+}
+
+
+
+
